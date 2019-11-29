39e4823b6f4284734b0e4806c97261e9fa93b27a
Muhammad Hanzala Qureshi <hanzalarohan@gmail.com>
2019-11-29 18:51:54 +0500

    css

--- src/App.js
+++ src/App.js
@@ -8,19 +8,27 @@ function App()
   const date = new Date()
   const hours = date.getHours()
    let timeOfDay=null
+   const styles=
+   {
+     fontSize:25
+   }
    if (hours > 12)
    {
      timeOfDay="Morning"
+     styles.color="yellow"
    }
    else if (hours>12 && hours < 17)
    {
      timeOfDay="Gof Fternoon"
+     styles.color = "green"
    }
    else
    {
      timeOfDay="GOod Morning"
+     styles.color = "blue"
    }
-  return (<h1>{timeOfDay} + {hours}</h1>);
+
+  return (<h1 className="xd">{timeOfDay} + {hours}</h1>);
 }
 
 export default App;
