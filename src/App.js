ba67ffd067a6537a7fecd21bfb611060b8555ed9
Muhammad Hanzala Qureshi <hanzalarohan@gmail.com>
2019-11-29 18:36:09 +0500

    cases

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() 
+{
+
+  const date = new Date()
+  const hours = date.getHours()
+   let timeOfDay=null
+   if (hours > 12)
+   {
+     timeOfDay="Morning"
+   }
+   else if (hours>12 && hours < 17)
+   {
+     timeOfDay="Gof Fternoon"
+   }
+   else
+   {
+     timeOfDay="GOod Morning"
+   }
+  return (<h1>{timeOfDay} + {hours}</h1>);
 }
 
 export default App;
